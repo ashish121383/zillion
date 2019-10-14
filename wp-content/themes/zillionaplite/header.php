beb46235491af8be2683fe657ccdf77986928a2c
ashish <ashish121383@gmail.com>
2019-10-14 18:51:26 +0530

    Add Header According to theme

--- wp-content/themes/zillionaplite/header.php
+++ wp-content/themes/zillionaplite/header.php
@@ -21,38 +21,54 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<header class="mainheader header-main">
+    <nav class="navbar navbar-expand-lg fixed-top">
+      <div class="container">
+		  <?php 
+		 	$custom_logo_id = get_theme_mod( 'custom_logo' );
+			$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+		  ?>
+        <a class="navbar-brand" href="<?php echo home_url(); ?>"><img src="<?php echo $image[0]; ?>" alt="Zillion"></a>
+        <button id="nav-icon1" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav justify-content-center mx-auto nav-zillion">
+            <li class="nav-item">
+              <a class="nav-link" href="experties.html">What we do <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about-us.html">Who are we</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="insights.html">Insights</a>
+            </li>
+               <li class="nav-item">
+              <a class="nav-link" href="our-people.html">Our People</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="event.html">Events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="join.html">Join us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Get In Touch</a>
+            </li>
+          </ul>
+          <div class="header-right-wrap mt-50">
+            <ul>
+              <li>
+                <a href="#"><span>Explore More</span> <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-search fa-w-16 fa-fw"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" class=""></path></svg></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+	</header>
+	
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'zillionaplite' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$zillionaplite_description = get_bloginfo( 'description', 'display' );
-			if ( $zillionaplite_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $zillionaplite_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'zillionaplite' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
 	<div id="content" class="site-content">
