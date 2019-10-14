3a5e88f117b7af8f51bb09464921c9ff3c0f1411
ashish <ashish121383@gmail.com>
2019-10-14 18:26:02 +0530

    Add and Remove all necessary js and css files

---
+++ wp-content/themes/zillionaplite/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+"use strict";
+
+   //* Navbar Fixed  
+    function navbarFixed(){
+        if ( $('.MainHeader').length ){ 
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll){
+                    $(".MainHeader").addClass("navbar-fixed");
+                } else {
+                    $(".MainHeader").removeClass("navbar-fixed");
+                }
+            });
+        };
+    };
+    navbarFixed();
+
+ $(document).ready(function() {
+            $('#nav-icon1').click(function() {
+                $(this).toggleClass('open');
+            });
+   });
+    
+
+
+ 
+})(jQuery);
